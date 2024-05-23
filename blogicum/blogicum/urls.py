be1d2eeaa746c4b51b4d86f492060747fbc027fa
Valentin Bobkov <ya333@yandex.ru>
2024-05-23 23:40:01 +0300

    final(pep8)

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('posts/<int:id>/', include('blog.urls', namespace='blog')),
-    path('category/<slug:category_slug>/', include('blog.urls', namespace='blog')),
+    path(
+        'category/<slug:category_slug>/',
+        include('blog.urls', namespace='blog')
+    ),
     path('pages/', include('pages.urls', namespace='pages')),
 ]
